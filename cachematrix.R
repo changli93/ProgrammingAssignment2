2c7b0ee7ae35ceec9a31d3e0206dab05160a06a5
changli93 <39235821+changli93@users.noreply.github.com>
2018-05-19 01:41:09 +0800

    R code for assignment 2
    matrix inverse computation based on lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,52 @@
-# makeCacheMatrix creates an object that caches its matrix inverse.
+## The following lexical scoping based functions are designed to create a special object that stores a matrix 
+## and caches the matrix inverse.
+
+## makeCacheMatrix function creates an object that caches its matrix inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  invmat <- NULL
+  # e.g. input parameter: x <- matrix(rnorm(9),3,3)
+  
+  # nothing is cached at the beginning, set null to inv_matrix variable
+  inv_matrix <- NULL
+  
+  # set the stored matrix value
   setmat <- function(y){
     x <<- y
-    invmat <<- NULL
+    inv_matrix <<- NULL
   }
-  getmat <- function() x
-  setInverse <- function(solveMatrix) invmat <<- solveMatrix
-  getInverse <- function() invmat
+  
+  # return the stored matrix
+  getmat <- function() x 
+  
+  # cache the given argument
+  setInverse <- function(solveMatrix) inv_matrix <<- solveMatrix
+  
+  # return the cached value (matrix inverse)
+  getInverse <- function() inv_matrix
+  
+  # each named element in the list indicates a function
   list(setmat = setmat, getmat = getmat, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-# cacheSolve computes matrix inverse from returned value of makeCacheMatrix function
+## cacheSolve function computes matrix inverse from returned value of makeCacheMatrix function
+## if matrix inverse has been computed before, it will return matrix inverse from cache
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  invmat <- x$getInverse()
-  if(!is.null(invmat)){
+  
+  # get cached value of matrix inverse
+  inv_matrix <- x$getInverse()
+   
+  # if the cached value is not null, return the cached value
+  if(!is.null(inv_matrix)){
     message("getting cached data")
-    return(invmat)
+    return(inv_matrix)
   }
+  
+  # otherwise, take that matrix and caclulate its inverse, then cache it
   mat_data <- x$getmat()
-  invmat <- solve(mat_data)
-  x$setInverse(invmat)
-  invmat      
+  inv_matrix <- solve(mat_data)
+  x$setInverse(inv_matrix)
+  
+  # return matrix inverse
+  inv_matrix      
 }
 
